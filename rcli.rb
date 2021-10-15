e8cac5b8c24ad889f763574fec65f628861c5378
Dan Wendorf <dan@render.com>
2021-10-15 13:28:01 -0700

    rcli: 0.0.1 -> 0.0.2

--- rcli.rb
+++ rcli.rb
@@ -1,9 +1,9 @@
 class Rcli < Formula
   desc 'CLI for working with Render'
   homepage 'https://github.com/wendorf/render-cli'
-  version '0.0.1'
+  version '0.0.2'
   url "https://github.com/wendorf/render-cli/releases/download/v#{version}/darwin_amd64.tar.gz"
-  sha256 'ed0277297e004b867680f042558697fa913445efd294c6bf8215deb607b9645a'
+  sha256 'cf1ae14824186460b473a4f0ea903af96a21e274d7660428e2b8f3187fdf9722'
 
   def install
     bin.install 'rcli'
